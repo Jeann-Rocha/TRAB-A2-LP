85076dae98b2daf0c7cc7755415772ef9a0ecf1e
Jeann-Rocha <jeannrochasilva@gmail.com>
2023-12-09 22:02:35 -0300

    Atualizando exceções e documentando as classes

--- exception_game.py
+++ exception_game.py
@@ -7,7 +7,43 @@ class MusicLoadError(Exception):
     Classe que detecta se houve erro ao carregar música.
     """
 
-    def __init__(self, message="Erro ao carregar música."):
+    def __init__(self, message="Erro ao carregar música.") -> None:
+        """
+        Método construtor da classe MusicLoadError.
+
+        Parameters
+        ----------
+        message: str (Opcional)
+            Mensagem de erro.
+
+        Returns
+        -------
+        None.
+        """
+
+        self.message = message
+        super().__init__(self.message)
+
+
+class SoundLoadError(Exception):
+    """
+    Classe que detecta se houve erro ao carregar efeito sonoro.
+    """
+
+    def __init__(self, message="Erro ao carregar efeito sonoro.") -> None:
+        """
+        Método construtor da classe SoundLoadError.
+
+        Parameters
+        ----------
+        message: str (Opcional)
+            Mensagem de erro.
+
+        Returns
+        -------
+        None.
+        """
+        
         self.message = message
         super().__init__(self.message)
 
@@ -17,7 +53,20 @@ class SpriteGroupError(Exception):
     Classe que detecta se houve erro ao criar grupos de sprites.
     """
     
-    def __init__(self, message="Erro ao criar grupos de sprites."):
+    def __init__(self, message="Erro ao criar grupos de sprites.") -> None:
+        """
+        Método construtor da classe SpriteGroupError.
+
+        Parameters
+        ----------
+        message: str (Opcional)
+            Mensagem de erro.
+
+        Returns
+        -------
+        None.
+        """
+        
         self.message = message
         super().__init__(self.message)
 
@@ -27,7 +76,20 @@ class GameLoopError(Exception):
     Classe que detecta se houve erro durante a execução do gameloop.
     """
     
-    def __init__(self, message="Erro durante a execução do gameloop."):
+    def __init__(self, message="Erro durante a execução do gameloop.") -> None:
+        """
+        Método construtor da classe GameLoopError.
+
+        Parameters
+        ----------
+        message: str (Opcional)
+            Mensagem de erro.
+
+        Returns
+        -------
+        None.
+        """
+        
         self.message = message
         super().__init__(self.message)
 
@@ -37,7 +99,20 @@ class SpriteInstanceError(Exception):
     Classe que detecta se houve erro ao criar instâncias de sprites.
     """
     
-    def __init__(self, message="Erro ao criar instâncias de sprites."):
+    def __init__(self, message="Erro ao criar instâncias de sprites.") -> None:
+        """
+        Método construtor da classe SpriteInstanceError.
+
+        Parameters
+        ----------
+        message: str (Opcional)
+            Mensagem de erro.
+
+        Returns
+        -------
+        None.
+        """
+        
         self.message = message
         super().__init__(self.message)
 
@@ -47,7 +122,20 @@ class CollisionError(Exception):
     Classe que detecta se houve erro durante a colisão de sprites.
     """
 
-    def __init__(self, message="Erro durante a colisão de sprites."):
+    def __init__(self, message="Erro durante a colisão de sprites.") -> None:
+        """
+        Método construtor da classe CollisionError.
+
+        Parameters
+        ----------
+        message: str (Opcional)
+            Mensagem de erro.
+
+        Returns
+        -------
+        None.
+        """
+        
         self.message = message
         super().__init__(self.message)
 
@@ -57,7 +145,20 @@ class UpdateScreenError(Exception):
     Classe que detecta se houve erro durante a atualização da tela.
     """
 
-    def __init__(self, message="Erro durante a atualização da tela."):
+    def __init__(self, message="Erro durante a atualização da tela.") -> None:
+        """
+        Método construtor da classe UpdateScreenError.
+
+        Parameters
+        ----------
+        message: str (Opcional)
+            Mensagem de erro.
+
+        Returns
+        -------
+        None.
+        """
+        
         self.message = message
         super().__init__(self.message)
     

bf80969754d3b962e8466a53608b54e9d45b4121
Alessandra Belló <alessandrabellosoares@gmail.com>
2023-12-09 22:00:39 -0300

    documentação player e obstaculos

--- sprites.py
+++ sprites.py
@@ -57,229 +57,357 @@ class Player(pg.sprite.Sprite, Render):
     Classe de Sprite(s) para o player (jogador) do jogo.
     """
 
-    def __init__(self, display: pg.Surface , scale: list, path_images: list, *groups, group_shoot: pg.sprite.Group) -> None:
+    def __init__(self, display: pg.Surface, scale: list, path_images: list, *groups, group_shoot: pg.sprite.Group) -> None:
+        """
+        Método constutor da classe Player.
+
+        Parameters
+        ----------
+        display : pg.Surface
+            Tela onde acontece o jogo.
+        scale : list
+            Lista contendo os valores x e y da escala do sprite.
+        path_images : list
+            Lista contendo o conjunto de imagens do sprite.
+        groups : pg.sprite.Group
+            Conjunto de grupos que o sprite pertence.
+        group_shoot: pg.sprite.Group
+            Sprite do tiro que será utilizado pelo player.
+        
+        Returns
+        -------
+        None.
+        """
+        
         pg.sprite.Sprite.__init__(self, *groups)
         Render.__init__(self, display, scale, path_images, *groups)
 
         # posição inicial do player
         self.rect.x = 0
-        self.rect.y = self.display.get_height() // 2
+        self.rect.y = self._display.get_height() // 2
 
-        self.timer_shoot = 0
-        self.timer_shoot_max = 10
-        self.group_shoot = group_shoot
+        self.__timer_shoot = 0
+        self.__timer_shoot_max = 8
+        self.__group_shoot = group_shoot
+
+        self.shooting_enabled = True
+        self.increase_speed_enabled = True
 
         self.damaged = False # indicador de que o player levou dano
         self.lifes = 3
-        self.speed = 30
-        self.animation_speed = 10
-
-        self.shooting_enabled = True
-        self.reset_timer = None
-
-        self.increase_speed_enabled = True
-        self.reset_speed_timer = None
+        self.__speed = 30
+        self._animation_speed = 10
 
     def update(self) -> None:
         """
         Método que atualiza os movimentos e tiros do player.
+                
+        Parameters
+        ----------
+
+        Returns
+        -------
+        None.
         """
 
         if self.damaged:
             self.damaged = False
-            # colocar som de dano no player...
         else:
-            self.display.blit(self.image, (self.rect.x, self.rect.y))
+            self._display.blit(self.image, (self.rect.x, self.rect.y))
 
-        self.keys = pg.key.get_pressed()
+        self.__keys = pg.key.get_pressed()
 
-        self.animate()
-        if self.animation_speed >= 2:
-            self.animation_speed -= 0.05 # efetio contínuo de aumento da velocidade
-        self.movements()
-        self.shoot_player()
-        self.draw_lifes()
+        self._animate()
+        if self._animation_speed >= 2:
+            self._animation_speed -= 0.05 # efetio contínuo de aumento da velocidade
+        self.__movements()
+        self.__shoot_player()
+        self.__draw_lifes()
 
-    def movements(self) -> None:
+    def __movements(self) -> None:
         """
         Método que atualiza os movimentos do player e limita os mesmos
         para as bordas da tela.
+                
+        Parameters
+        ----------
+
+        Returns
+        -------
+        None.
         """
 
-        # diagonal superior esquerda
-        if self.keys[K_w] and self.keys[K_a]:
-            self.rect.x -= self.speed
-            self.rect.y -= self.speed
-        # diagonal superior direita
-        elif self.keys[K_w] and self.keys[K_d]:
-            self.rect.x += self.speed
-            self.rect.y -= self.speed
-        # diagonal inferior esquerda
-        elif self.keys[K_s] and self.keys[K_a]:
-            self.rect.x -= self.speed
-            self.rect.y += self.speed
-        # diagonal inferior direita
-        elif self.keys[K_s] and self.keys[K_d]:
-            self.rect.x += self.speed
-            self.rect.y += self.speed
-        # cima
-        elif self.keys[K_w]:
-            self.rect.y -= self.speed
-        # baixo
-        elif self.keys[K_s]:
-            self.rect.y += self.speed
-        # esquerda
-        elif self.keys[K_a]:
-            self.rect.x -= self.speed
-        # direita
-        elif self.keys[K_d]:
-            self.rect.x += self.speed
-
-        # borda superior
+        # Diagonal superior esquerda
+        if self.__keys[K_w] and self.__keys[K_a]:
+            self.rect.x -= self.__speed
+            self.rect.y -= self.__speed
+        # Diagonal superior direita
+        elif self.__keys[K_w] and self.__keys[K_d]:
+            self.rect.x += self.__speed
+            self.rect.y -= self.__speed
+        # Diagonal inferior esquerda
+        elif self.__keys[K_s] and self.__keys[K_a]:
+            self.rect.x -= self.__speed
+            self.rect.y += self.__speed
+        # Diagonal inferior direita
+        elif self.__keys[K_s] and self.__keys[K_d]:
+            self.rect.x += self.__speed
+            self.rect.y += self.__speed
+        # Cima
+        elif self.__keys[K_w]:
+            self.rect.y -= self.__speed
+        # Baixo
+        elif self.__keys[K_s]:
+            self.rect.y += self.__speed
+        # Esquerda
+        elif self.__keys[K_a]:
+            self.rect.x -= self.__speed
+        # Direita
+        elif self.__keys[K_d]:
+            self.rect.x += self.__speed
+        # Borda superior
         if self.rect.top < 0:
             self.rect.top = 0
-        # borda inferior
-        if self.rect.bottom > self.display.get_height():
-            self.rect.bottom = self.display.get_height()
-        # borda lateral esquerda
+        # Borda inferior
+        if self.rect.bottom > self._display.get_height():
+            self.rect.bottom = self._display.get_height()
+        # Borda lateral esquerda
         if self.rect.left < 0:
             self.rect.left = 0
-        # borda lateral direita
-        if self.rect.right > self.display.get_width():
-            self.rect.right = self.display.get_width()
+        # Borda lateral direita
+        if self.rect.right > self._display.get_width():
+            self.rect.right = self._display.get_width()
 
-    def draw_lifes(self) -> None:
+    def __draw_lifes(self) -> None:
         """
         Método que desenha no canto superior esquerdo da tela as vidas que o player tem.
+                
+        Parameters
+        ----------
+
+        Returns
+        -------
+        None.
         """
 
-        life = Render(self.display, cst.SCALE_LIFE, cst.LIFE, self.groups[0])
+        life = Render(self._display, cst.SCALE_LIFE, cst.ITEM_LIFE, self._groups[0])
 
         for n in range(self.lifes):
-            life.display.blit(life.image, (20 + 50 * n, 20))
+            life._display.blit(life.image, (20 + 50 * n, 20))
 
-    def shoot_player(self) -> None:
+    def __shoot_player(self) -> None:
         """
         Método que atualiza os tiros do player.
+                
+        Parameters
+        ----------
+
+        Returns
+        -------
+        None.
         """
 
-        self.timer_shoot += 1
-        if self.timer_shoot > self.timer_shoot_max:
-            if self.keys[K_j]:
-                self.timer_shoot = 0
-                Shoot(self.display, cst.SCALE_SHOOT, cst.SHOOT_PLAYER, self.rect.topright, self.speed, False, (self.groups[0], self.group_shoot))
-
+        self.__timer_shoot += 1
+        if self.__timer_shoot > self.__timer_shoot_max:
+            if self.__keys[K_j]:
+                self.__timer_shoot = 0
+                Shoot(self._display, cst.SCALE_SHOOT, cst.SHOOT_PLAYER, self.rect.topright, self.__speed, False, (self._groups[0], self.__group_shoot))
 
     def increase_fire_rate(self) -> None:
         """
         Método para aumentar temporariamente a taxa de tiro.
-        """
-        self.shooting_enabled = False
-        self.timer_shoot_max = 2
+                
+        Parameters
+        ----------
 
-        # Inicia um temporizador para voltar ao timer_shoot_max após 10 segundos
-        self.reset_timer = threading.Timer(10, self._reset_fire_rate)
+        Returns
+        -------
+        None.
+        """
+
+        self.shooting_enabled = False
+        self.__timer_shoot_max = 1
+
+        # Inicia um temporizador para voltar ao tempo de tiro original após 15 segundos
+        self.reset_timer = threading.Timer(15, self.__reset_fire_rate)
         self.reset_timer.start()
 
-    def _reset_fire_rate(self) -> None:
+    def __reset_fire_rate(self) -> None:
         """
         Método chamado pelo temporizador para reverter as alterações após a duração.
+                
+        Parameters
+        ----------
+
+        Returns
+        -------
+        None.
         """
-        self.timer_shoot_max = 10
+
+        self.__timer_shoot_max = 8
         self.shooting_enabled = True
 
     def increase_speed(self) -> None:
         """
         Método para aumentar temporariamente a velocidade do player.
-        """
-        self.increase_speed_enabled = False
-        self.speed = 50
-        self.animation_speed = 1
+                
+        Parameters
+        ----------
 
-        # Inicia um temporizador para voltar ao speed após 10 segundos
-        self.reset_speed_timer = threading.Timer(10, self._reset_speed)
+        Returns
+        -------
+        None.
+        """
+
+        self.increase_speed_enabled = False
+        self.__speed = 60
+        self._animation_speed = 1
+
+        # Inicia um temporizador para voltar a velocidade original após 15 segundos
+        self.reset_speed_timer = threading.Timer(15, self.__reset_speed)
         self.reset_speed_timer.start()
 
-    def _reset_speed(self) -> None:
+    def __reset_speed(self) -> None:
         """
         Método chamado pelo temporizador para reverter as alterações após a duração.
+                
+        Parameters
+        ----------
+
+        Returns
+        -------
+        None.
         """
-        self.speed = 30
+
+        self.__speed = 30
         self.increase_speed_enabled = True
+        self._animation_speed = 2
 
     def increase_hearth(self) -> None:
         """
         Método para aumentar a vida do player.
+                
+        Parameters
+        ----------
+
+        Returns
+        -------
+        None.
         """
+
         self.lifes += 1
+
 
 class Obstacle(pg.sprite.Sprite, Render):
     """
     Classe de Sprite(s) para os obstáculos do jogo.
     """
-    
+
     is_boss = False # atríbuto global para indicar se há um boss (obstáculos e boss não atuam simultaneamente)
 
     def __init__(self, display: pg.Surface, scale: list, path_images: list, speed_increment: float, *groups, group_shoot: pg.sprite.Group) -> None:
+        """
+        Método constutor da classe Obstacle.
+
+        Parameters
+        ----------
+        display : pg.Surface
+            Tela onde acontece o jogo.
+        scale : list
+            Lista contendo os valores x e y da escala do sprite.
+        path_images : list
+            Lista contendo o conjunto de imagens do sprite.
+        speed_increment : float
+            Incremento na velocidade do obstáculo.
+        groups : pg.sprite.Group
+            Conjunto de grupos que o sprite pertence.
+        group_shoot: pg.sprite.Group
+            Sprite do tiro que será utilizado pelo obstáculo.
+        
+        Returns
+        -------
+        None.
+        """
+        
         pg.sprite.Sprite.__init__(self, *groups)
         Render.__init__(self, display, scale, path_images, *groups)
 
-        self.group_shoot = group_shoot
-        self.rect.x = self.display.get_width()
+        self.__group_shoot = group_shoot
+        self.rect.x = self._display.get_width()
         self.rect.y = random.randint(0, display.get_height() - scale[1]) # posição aleatória em relação a altura da tela
-        self.timer_shoot = 0
-        self.timer_shoot_max = 50
-        self.min_speed, self.max_speed = 20, 30 # constantes que randomizam a velocidade dos obstáculos
-        self.speed_increment = speed_increment
-        self.speed = speed_increment / 5 + random.randint(self.min_speed, self.max_speed)
 
+        self.timer_shoot = 0
+        self.__timer_shoot_max = 50
+        self.__min_speed, self.__max_speed = 20, 30 # constantes que randomizam a velocidade dos obstáculos
+        self.__speed_increment = speed_increment
+        self.speed = self.__speed_increment / 5 + random.randint(self.__min_speed, self.__max_speed)
 
     def update(self) -> None:
         """
         Método que atualiza os movimentos dos obstáculos, que estão
         pré-definidos pelo jogo.
+                
+        Parameters
+        ----------
+
+        Returns
+        -------
+        None.
         """
 
         if not self.rect.right < 0 and not self.exploded:
-            self.display.blit(self.image, (self.rect.x, self.rect.y))
+            self._display.blit(self.image, (self.rect.x, self.rect.y))
 
             self.rect.x -= self.speed
 
             if not self.is_boss:
-                self.new_obstacles()
-            self.shoot_obstacles()
-            self.animate()
+                self.__new_obstacles()
+            self.__shoot_obstacles()
+            self._animate()
         elif self.exploded: # o surgimento do boss explode os obstáculos existentes na tela
-            self.display.blit(self.image, (self.rect.x, self.rect.y))
-            self.animate_explosion()
+            self._display.blit(self.image, (self.rect.x, self.rect.y))
+            self._animate_explosion()
 
         if self.rect.right < 0 or self.exploded:
-            self.groups[1].remove(self)
+            self._groups[1].remove(self)
 
-    def new_obstacles(self) -> None:
+    def __new_obstacles(self) -> None:
         """
         Método que gera (aleatoriamente) novos obstáculos.
+                
+        Parameters
+        ----------
+
+        Returns
+        -------
+        None.
         """
 
         new_obstacles = random.randint(1, 4) # quantidade aleatória entre 1 e 4 obstáculos a ser gerada
-        if len(self.groups[1].sprites()) <= 2: # condição para ter mais obstáculos (veja que o máximo tem que ser 2 + 4 = 6)
+        if len(self._groups[1].sprites()) <= 2: # condição para ter mais obstáculos (veja que o máximo tem que ser 2 + 4 = 6)
             if random.random() < 0.03: # probabilidade de 3% de gerar mais obstáculos
                 for _ in range(new_obstacles):
-                    Obstacle(self.display, cst.SCALE_OBSTACLE, cst.OBSTACLE, self.speed_increment, (self.groups[0], self.groups[1]), group_shoot=self.group_shoot)
+                    Obstacle(self._display, cst.SCALE_OBSTACLE, cst.OBSTACLE, self.__speed_increment, (self._groups[0], self._groups[1]), group_shoot=self.__group_shoot)
 
-
-    def shoot_obstacles(self) -> None:
+    def __shoot_obstacles(self) -> None:
         """
         Método que atualiza os tiros do obstáculo.
+                
+        Parameters
+        ----------
+
+        Returns
+        -------
+        None.
         """
 
         self.timer_shoot += 1
-        if self.timer_shoot > self.timer_shoot_max:
+        if self.timer_shoot > self.__timer_shoot_max:
             self.timer_shoot = 0
-            obstacles_choice = random.sample(self.groups[1].sprites(), random.randint(0, len(self.groups[1].sprites()))) # escolhe uma amostra da quantidade de obstáculos na tela para atirar
+            obstacles_choice = random.sample(self._groups[1].sprites(), random.randint(0, len(self._groups[1].sprites()))) # escolhe uma amostra da quantidade de obstáculos na tela para atirar
             for oc in obstacles_choice:
-                Shoot(self.display, cst.SCALE_SHOOT, cst.SHOOT_OBSTACLE, (oc.rect.left, oc.rect.centery), oc.speed, True, (self.groups[0], self.group_shoot))
-            for ob in self.groups[1].sprites():
+                Shoot(self._display, cst.SCALE_SHOOT, cst.SHOOT_OBSTACLE, (oc.rect.left, oc.rect.centery), oc.speed, True, (self._groups[0], self.__group_shoot))
+            for ob in self._groups[1].sprites():
                 ob.timer_shoot = 0
 
 
